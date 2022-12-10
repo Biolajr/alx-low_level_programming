4907bc6532e8ad8c27e24819b29581d27c0c0fa6
Biolajr <Biola.lawaljr@gmail.com>
2022-12-09 20:36:47 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,10 @@ int main(void)
 	int tens;
 	int tens;
 
-	for (ones = 0; tens <= 9; tens++)
+	for (tens = 0; tens <= 9; tens++)
 	{
 		for (ones = tens + 1; ones <= 9; ones++)
+		{
 			putchar(tens + '0');
 			putchar(ones + '0');
 			if (tens < 8)

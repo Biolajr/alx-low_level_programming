e8b3dd12a6a3c75f9b3be48df04914fd50922890
Biolajr <Biola.lawaljr@gmail.com>
2022-12-09 18:41:52 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,15 +2,15 @@
 
 /**
  * main - Entry point
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-		putchr(c);
-	for (c = 'A'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
 		putchar(c);
 	putchar("\n");
 	return (0);

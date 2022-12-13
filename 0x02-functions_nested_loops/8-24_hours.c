0b07041a6811ba928e34088f3a7b02638e4cbfef
Biolajr <Biola.lawaljr@gmail.com>
2022-12-13 09:51:31 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,3 +17,20 @@ void jack_bauer(void)
 			_putchar(':');
 			_putchar((minute / 10) + '0');
 }
+
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+

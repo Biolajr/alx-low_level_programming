7135c4b7db1f2747c82ad0aed715b635b27458e9
Biolajr <Biola.lawaljr@gmail.com>
2022-12-15 16:57:49 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,25 +12,25 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b)
-{
-if (a > c)
-{
-largest = a;
-}
-else
-{
-largest = c;
-}
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
+	if (a > b)
+	{
+	if (a > c)
+	{
+	largest = a;
+	}
+	else
+	{
+	largest = c;
+	}
+	}
+	else if (b > c)
+	{
+	largest = b;
+	}
+	else
+	{
+	largest = c;
+	}
 
-return (largest);
+	return (largest);
 }
